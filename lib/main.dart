9d3ba1919140ff0f10dbfb7543a44928af06cd6d
Muhless <nuryadi.muhta@gmail.com>
2024-12-12 23:44:49 +0700

    [UPDATE]

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +12,32 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Rental.in',
+            style: GoogleFonts.roboto(textStyle: TextStyle(fontSize: 25)),
+          ),
+          backgroundColor: Colors.amber,
+        ),
         body: Center(
-          child: Text('cihuy'),
+          child: Column(
+            children: [
+              Text(
+                'Rental.In',
+                style: (TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                )),
+              ),
+              Text('pilih mobil'),
+              Image.asset('assets/img/R.png'),
+              Text('Lambogini palabapakkau'),
+              Text('cihuy')
+            ],
+          ),
         ),
       ),
     );
