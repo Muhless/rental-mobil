61f89d02859889f1196b37afd307360be7fbf6bd
Muhless <nuryadi.muhta@gmail.com>
2024-12-16 23:55:14 +0700

    [UPDATE] update list mobil

--- lib/screen/mobil/keluarga/keluarga.dart
+++ lib/screen/mobil/keluarga/keluarga.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:rental_mobil/screen/home.dart';
 import 'package:rental_mobil/screen/mobil/keluarga/detail.dart';
+import 'package:rental_mobil/widgets/search.dart';
 
 class MobilKeluarga extends StatefulWidget {
   const MobilKeluarga({super.key});
@@ -17,79 +18,168 @@ class _MobilKeluargaState extends State<MobilKeluarga> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.grey[50],
+        backgroundColor: Color(0xff222831),
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 250.0,
-                decoration: BoxDecoration(
-                  color: Colors.lime,
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20)),
-                ),
-                child: Image.asset('assets/R.png'),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Row(
                   children: [
-                    Text(
-                      'Tipe Mobil',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => HomeScreen()),
-                        );
-                      },
-                      child: Icon(
-                        Icons.arrow_forward_sharp,
-                        size: 30,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              // list mobil 1
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      GestureDetector(
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 20),
+                      child: GestureDetector(
                         onTap: () {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => DetailMobilKeluarga()),
+                                builder: (context) => HomeScreen()),
                           );
                         },
-                        child: Container(
-                          width: 250,
-                          height: 200.0,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Image.asset('assets/R.png'),
+                        child: Icon(
+                          Icons.arrow_back,
+                          size: 30,
+                          color: Colors.white,
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                Row(
+                  children: [SearchExampleScreen()],
+                ),
+                // list mobil 1
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DetailMobilKeluarga()),
+                        );
+                      },
+                      child: Container(
+                        width: 350,
+                        height: 200.0,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  'assets/icon/toyota.png',
+                                  height: 50,
+                                  width: 100,
+                                ),
+                                const SizedBox(height: 8),
+                                Text(
+                                  'Toyota',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                const Text(
+                                  'Avanza',
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.asset(
+                                  'assets/R.png',
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
+      ),
+    );
+    // batas list
+    //
+  }
+}
+
+class SearchExampleScreen extends StatefulWidget {
+  @override
+  _SearchExampleScreenState createState() => _SearchExampleScreenState();
+}
+
+class _SearchExampleScreenState extends State<SearchExampleScreen> {
+  // Data awal
+  List<String> allData = [
+    'Avanza',
+    'Xenia',
+    'Innova',
+    'Fortuner',
+    'Brio',
+    'HRV'
+  ];
+  List<String> filteredData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    filteredData = allData; // Set data awal
+  }
+
+  void filterSearch(String query) {
+    final results = allData
+        .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+    setState(() {
+      filteredData = results; // Update hasil filter
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Custom Search Example'),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Column(
+        children: [
+          CustomSearchBar(
+            onSearchChanged: filterSearch, // Gunakan callback pencarian
+            hintText: 'Cari nama mobil...',
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredData.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: const Icon(Icons.directions_car),
+                  title: Text(filteredData[index]),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
