fab68f7382d6879d82d09991ee123413f7aba2b7
Muhless <nuryadi.muhta@gmail.com>
2024-12-21 23:33:44 +0700

    [UPDATE] update homescreen

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:rental_mobil/screen/mobil/keluarga/detail.dart';
 import 'package:rental_mobil/screen/mobil/keluarga/list.dart';
 import 'package:rental_mobil/widgets/statusbar.dart';
 import 'package:rental_mobil/widgets/warna.dart';
@@ -30,7 +31,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     Align(
                       alignment: Alignment.topLeft,
                       child: Text(
-                        'Hello,',
+                        'Halo,',
                         style: TextStyle(
                             fontSize: 20,
                             fontWeight: FontWeight.w300,
@@ -45,29 +46,34 @@ class _HomeScreenState extends State<HomeScreen> {
                           'Budiono Siregar',
                           style: TextStyle(fontSize: 30, color: Colors.white),
                         ),
-                        Icon(
-                          Icons.logout,
-                          size: 30,
-                          color: Colors.white,
+                        IconButton(
+                          icon: Icon(
+                            Icons.logout,
+                            size: 20,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            // Proses logout
+                            // _logout(context);
+                          },
                         )
                       ],
                     )
                   ],
                 ),
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 30),
               Column(
                 children: [
                   Container(
                     margin: EdgeInsets.only(left: 20, right: 20, top: 20),
                     child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: List.generate(
                         2,
                         (index) {
                           Icon iconSpesifikasi;
                           String judulSpesifikasi;
-                          String keteranganSpesifikasi;
 
                           switch (index) {
                             case 0:
@@ -76,169 +82,48 @@ class _HomeScreenState extends State<HomeScreen> {
                                 size: 40,
                                 color: Colors.white,
                               );
-                              judulSpesifikasi = 'Kapasitas';
-                              keteranganSpesifikasi = '4 Orang';
+                              judulSpesifikasi = 'Cihuy';
                               break;
                             case 1:
                               iconSpesifikasi = Icon(
-                                Icons.grid_goldenratio,
+                                Icons.format_list_bulleted_outlined,
                                 size: 40,
                                 color: Colors.white,
                               );
                               judulSpesifikasi = 'Type';
-                              keteranganSpesifikasi = 'Manual';
                               break;
                             default:
                               iconSpesifikasi = Icon(Icons.ac_unit);
                               judulSpesifikasi = 'Kapasitas';
-                              keteranganSpesifikasi = '4 Orang';
-                          }
-
-                          return Container(
-                            height: 150,
-                            width: 150,
-                            decoration: BoxDecoration(
-                              color: Warna.fourthColor,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                iconSpesifikasi,
-                                const SizedBox(height: 15),
-                                Text(
-                                  judulSpesifikasi,
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w300,
-                                      fontSize: 17,
-                                      color: Colors.white),
-                                ),
-                                Text(
-                                  keteranganSpesifikasi,
-                                  style: TextStyle(
-                                      fontSize: 20, color: Colors.white),
-                                ),
-                              ],
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: List.generate(
-                        2,
-                        (index) {
-                          Icon iconSpesifikasi;
-                          String judulSpesifikasi;
-                          String keteranganSpesifikasi;
-
-                          switch (index) {
-                            case 0:
-                              iconSpesifikasi = Icon(
-                                Icons.airline_seat_recline_extra,
-                                size: 40,
-                                color: Colors.white,
-                              );
-                              judulSpesifikasi = 'Kapasitas';
-                              keteranganSpesifikasi = '4 Orang';
-                              break;
-                            case 1:
-                              iconSpesifikasi = Icon(
-                                Icons.grid_goldenratio,
-                                size: 40,
-                                color: Colors.white,
-                              );
-                              judulSpesifikasi = 'Type';
-                              keteranganSpesifikasi = 'Manual';
-                              break;
-                            default:
-                              iconSpesifikasi = Icon(Icons.ac_unit);
-                              judulSpesifikasi = 'Kapasitas';
-                              keteranganSpesifikasi = '4 Orang';
-                          }
-
-                          return Container(
-                            height: 150,
-                            width: 150,
-                            decoration: BoxDecoration(
-                              color: Warna.fourthColor,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                iconSpesifikasi,
-                                const SizedBox(height: 15),
-                                Text(
-                                  judulSpesifikasi,
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w300,
-                                      fontSize: 17,
-                                      color: Colors.white),
-                                ),
-                                Text(
-                                  keteranganSpesifikasi,
-                                  style: TextStyle(
-                                      fontSize: 20, color: Colors.white),
-                                ),
-                              ],
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: List.generate(
-                        2,
-                        (index) {
-                          Icon iconSpesifikasi;
-                          String judulSpesifikasi;
-                          String keteranganSpesifikasi;
-
-                          switch (index) {
-                            case 0:
-                              iconSpesifikasi = Icon(
-                                Icons.airline_seat_recline_extra,
-                                size: 40,
-                                color: Colors.white,
-                              );
-                              judulSpesifikasi = 'Kapasitas';
-                              keteranganSpesifikasi = '4 Orang';
-                              break;
-                            case 1:
-                              iconSpesifikasi = Icon(
-                                Icons.grid_goldenratio,
-                                size: 40,
-                                color: Colors.white,
-                              );
-                              judulSpesifikasi = 'Type';
-                              keteranganSpesifikasi = 'Manual';
-                              break;
-                            default:
-                              iconSpesifikasi = Icon(Icons.ac_unit);
-                              judulSpesifikasi = 'Kapasitas';
-                              keteranganSpesifikasi = '4 Orang';
                           }
 
                           return GestureDetector(
                             onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => MobilKeluarga()),
-                              );
+                              switch (index) {
+                                case 0:
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => MobilKeluarga()),
+                                  );
+                                  break;
+                                case 1:
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            DetailMobilKeluarga()),
+                                  );
+                                  break;
+                                default:
+                                  Navigator.pop(
+                                    context,
+                                  );
+                              }
                             },
                             child: Container(
-                              height: 150,
-                              width: 150,
+                              height: 130,
+                              width: 130,
                               decoration: BoxDecoration(
                                 color: Warna.fourthColor,
                                 borderRadius: BorderRadius.circular(10),
@@ -255,10 +140,171 @@ class _HomeScreenState extends State<HomeScreen> {
                                         fontSize: 17,
                                         color: Colors.white),
                                   ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: List.generate(
+                        2,
+                        (index) {
+                          Icon iconSpesifikasi;
+                          String judulSpesifikasi;
+
+                          switch (index) {
+                            case 0:
+                              iconSpesifikasi = Icon(
+                                Icons.car_rental,
+                                size: 40,
+                                color: Colors.white,
+                              );
+                              judulSpesifikasi = 'Mobil Keluarga';
+                              break;
+                            case 1:
+                              iconSpesifikasi = Icon(
+                                Icons.fire_truck,
+                                size: 40,
+                                color: Colors.white,
+                              );
+                              judulSpesifikasi = 'Mobil Pickup';
+                              break;
+                            default:
+                              iconSpesifikasi = Icon(Icons.ac_unit);
+                              judulSpesifikasi = 'Kapasitas';
+                          }
+
+                          return GestureDetector(
+                            onTap: () {
+                              switch (index) {
+                                case 0:
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => MobilKeluarga()),
+                                  );
+                                  break;
+                                case 1:
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            DetailMobilKeluarga()),
+                                  );
+                                  break;
+                                default:
+                                  Navigator.pop(
+                                    context,
+                                  );
+                              }
+                            },
+                            child: Container(
+                              height: 130,
+                              width: 130,
+                              decoration: BoxDecoration(
+                                color: Warna.fourthColor,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  iconSpesifikasi,
+                                  const SizedBox(height: 15),
                                   Text(
-                                    keteranganSpesifikasi,
+                                    judulSpesifikasi,
                                     style: TextStyle(
-                                        fontSize: 20, color: Colors.white),
+                                        fontWeight: FontWeight.w300,
+                                        fontSize: 17,
+                                        color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: List.generate(
+                        2,
+                        (index) {
+                          Icon iconSpesifikasi;
+                          String judulSpesifikasi;
+
+                          switch (index) {
+                            case 0:
+                              iconSpesifikasi = Icon(
+                                Icons.account_circle_rounded,
+                                size: 40,
+                                color: Colors.white,
+                              );
+                              judulSpesifikasi = 'Profile';
+                              break;
+                            case 1:
+                              iconSpesifikasi = Icon(
+                                Icons.settings,
+                                size: 40,
+                                color: Colors.white,
+                              );
+                              judulSpesifikasi = 'Settings';
+                              break;
+                            default:
+                              iconSpesifikasi = Icon(Icons.ac_unit);
+                              judulSpesifikasi = 'Kapasitas';
+                          }
+
+                          return GestureDetector(
+                            onTap: () {
+                              switch (index) {
+                                case 0:
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => MobilKeluarga()),
+                                  );
+                                  break;
+                                case 1:
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            DetailMobilKeluarga()),
+                                  );
+                                  break;
+                                default:
+                                  Navigator.pop(
+                                    context,
+                                  );
+                              }
+                            },
+                            child: Container(
+                              height: 130,
+                              width: 130,
+                              decoration: BoxDecoration(
+                                color: Warna.fourthColor,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  iconSpesifikasi,
+                                  const SizedBox(height: 15),
+                                  Text(
+                                    judulSpesifikasi,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w300,
+                                        fontSize: 17,
+                                        color: Colors.white),
                                   ),
                                 ],
                               ),
